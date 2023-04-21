233e9df2b1c386d4fd8e03f791bc78820f71e46a
Jiří Málek <jiri.malek@gmail.com>
2023-04-21 12:28:29 +0200

    [fzf] Ignores shellcheck on fzf system files.

--- modules/fzf/autorun.zsh
+++ modules/fzf/autorun.zsh
@@ -8,11 +8,14 @@
 
     case "${DOTFILES_OS}" in
         "${DOTFILES_OS_MACOS}")
+            # shellcheck disable=SC1091
             [[ -f "${HOME}/.fzf.zsh" ]] && source "${HOME}/.fzf.zsh"
         ;;
 
         "${DOTFILES_OS_LINUX}")
+            # shellcheck disable=SC1091
             [[ -f /usr/share/zsh/site-functions/fzf ]] && source /usr/share/zsh/site-functions/fzf
+            # shellcheck disable=SC1091
             [[ -f /usr/share/fzf/shell/key-bindings.zsh ]] && source /usr/share/fzf/shell/key-bindings.zsh
         ;;
     esac
