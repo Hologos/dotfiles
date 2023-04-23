f2209dff013fdb20a6a0dea03863f98aa8e6b18a
Jiří Málek <jiri.malek@gmail.com>
2023-04-23 12:19:26 +0200

    [installation] Moves 'running generic installer' header to caller scripts.

--- installation/install.sh
+++ installation/install.sh
@@ -1,8 +1,5 @@
 #! /usr/bin/env bash
 
-echo
-echo "Running generic installer"
-
 find "${DOTFILES_MODULES_ROOT}" -name "install.sh" |
     while read -r module_install_file; do
         . "${module_install_file}"
